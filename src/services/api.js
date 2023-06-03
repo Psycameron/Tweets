4d3cec2fc2ec5e15956d646befc9b8f2a783cc97
Dmitriy Bulakhov <bulakhovda@gmail.com>
2023-06-03 19:11:13 +0300

    +

--- src/services/api.js
+++ src/services/api.js
@@ -1,19 +1,19 @@
 import axios from "axios";
 
-axios.defaults.baseURL = "https://64787a93362560649a2ddd8a.mockapi.io";
+axios.defaults.baseURL = "https://64787a93362560649a2ddd8a.mockapi.io/users";
 
 export const getAllUsers = async () => {
-  const { data } = await axios.get("/users");
+  const { data } = await axios.get("/");
 
   return data;
 };
 
 export const updateUser = async (user) => {
-  await axios.put(`/users/${user.id}`, user);
+  await axios.put(`/${user.id}`, user);
 };
 
 export const getUsers = async (page, limit) => {
-  const params = `/users?page=${page}&limit=${limit}`;
+  const params = `?page=${page}&limit=${limit}`;
   const { data } = await axios.get(params);
 
   return data;
